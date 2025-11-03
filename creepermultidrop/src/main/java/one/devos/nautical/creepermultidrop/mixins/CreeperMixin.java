410e89bf29b4d52937917197178f96689ae0e957
Oliver-makes-code <oliver@olivermakesco.de>
2025-11-03 16:00:36 -0600

    crepper

--- creepermultidrop/src/main/java/one/devos/nautical/creepermultidrop/mixins/CreeperMixin.java
+++ creepermultidrop/src/main/java/one/devos/nautical/creepermultidrop/mixins/CreeperMixin.java
@@ -2,6 +2,8 @@ package one.devos.nautical.creepermultidrop.mixins;
 
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Creeper;
@@ -16,13 +18,38 @@ public abstract class CreeperMixin extends Entity {
     @Shadow
     public abstract boolean isPowered();
 
+    @Shadow private boolean droppedSkulls;
+
     public CreeperMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
 
-    @ModifyReturnValue(method = "canDropMobsSkull", at = @At("RETURN"))
-    public boolean canDropMobsSkull(boolean original) {
-        // this is weirdly written im aware but it wasnt dropping 100% of the time on charged creeper which wtf? so.. `this.isPowered()` had to be added back
-        return original || this.random.nextFloat() < 0.1 || this.isPowered();
+//    @ModifyReturnValue(method = "canDropMobsSkull", at = @At("RETURN"))
+//    public boolean canDropMobsSkull(boolean original) {
+//        // this is weirdly written im aware but it wasnt dropping 100% of the time on charged creeper which wtf? so.. `this.isPowered()` had to be added back
+//        return original || this.random.nextFloat() < 0.1 || this.isPowered();
+//    }
+
+    @WrapOperation(
+            method = "killedEntity",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/monster/Creeper;isPowered()Z"
+            )
+    )
+    public boolean checkRandomChance(Creeper instance, Operation<Boolean> original) {
+        boolean isPowered = original.call(instance);
+
+        if (isPowered) {
+            this.droppedSkulls = false;
+            return true;
+        }
+
+        boolean randomChance = this.random.nextFloat() < 0.1;
+
+        if (!randomChance)
+            this.droppedSkulls = true;
+
+        return randomChance;
     }
 }
